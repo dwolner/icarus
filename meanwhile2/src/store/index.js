e55aa438a4fe7b736fc46b39fcbc3695eb583b58
Dan Wolner <daniel.wolner@gmail.com>
2020-03-11 13:55:19 -0700

    updated meanwhile and removed projectelias

--- meanwhile2/src/store/index.js
+++ meanwhile2/src/store/index.js
@@ -20,6 +20,26 @@ export default function(/* { ssrContext } */) {
         windowWidth: '',
         content: [
             {
+              imageFilename: "zer0.jpg",
+              // overlayFilename: "",
+              title: "KREWELLA - ZERO TRAILER",
+              tags: ['featured', 'music'],
+              embedURL:
+                "https://www.youtube.com/embed/HP28eanVPJA",
+              description:
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            },
+            {
+              imageFilename: "seance.jpg",
+              // overlayFilename: "",
+              title: "IMAN & TEYANA TAYLOR - SÉANCE",
+              tags: ['featured', 'music'],
+              embedURL:
+                "https://www.youtube.com/embed/64gU5EvLEz8",
+              description:
+                "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+            },
+            {
               imageFilename: "riverrundry.png",
               // overlayFilename: "",
               title: "RIVER RUN DRY",
@@ -173,7 +193,7 @@ export default function(/* { ssrContext } */) {
                 imageFilename: "STARRO.jpg",
                 // overlayFilename: "handwriting/StarroFantasy.png",
                 title: "STARRO - FANTASY",
-                tags: ['music', 'featured'],
+                tags: ['music'],
                 embedURL:
                 "https://player.vimeo.com/video/236974574?color=ffffff&autoplay=1",
                 description:
@@ -204,7 +224,7 @@ export default function(/* { ssrContext } */) {
               // overlayFilename: "handwriting/HuntrHigher.png",
               title: "HUNTR - HIGHER",
               embedURL: "https://www.youtube.com/embed/0uISF_lQVqc",
-              tags: ['music', 'vfx', 'featured'],
+              tags: ['music', 'vfx'],
               description:
                 "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
             },
